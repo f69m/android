fa9bc8c2be25a599d7d2206c3368564974d167dd
f69m <git+commit@f69m.de>
2014-02-13 19:59:59 +0100

    Add scripts for picking cherries from OmniROM Gerrit

---
+++ pick-cherries.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+# [1/2] libc: support using old property system via makefile flag
+# URL: https://gerrit.omnirom.org/3389
+( cd bionic; \
+  git fetch https://gerrit.omnirom.org/android_bionic refs/changes/89/3389/3 && \
+  git cherry-pick FETCH_HEAD )
+
+# [2/2] init: support using old property system via makefile flag
+# URL: https://gerrit.omnirom.org/3390
+( cd system/core; \
+  git fetch https://gerrit.omnirom.org/android_system_core refs/changes/90/3390/2 && \
+  git cherry-pick FETCH_HEAD )
+
---
+++ update-cherries.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import json
+import os
+import urllib.request
+
+
+# **********************************************************************
+# Config
+
+URL_GERRIT = 'https://gerrit.omnirom.org'
+
+CHERRY_CONFIG = 'manifest/cherries.txt'
+CHERRY_SCRIPT = 'manifest/pick-cherries.sh'
+
+
+# **********************************************************************
+# Read cherries from config file
+
+gCherries = list()
+
+def readCherries():
+    with open(CHERRY_CONFIG, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if (len(line) == 0) or (line[0] == '#'):
+                continue
+            gCherries.append(line.split(' '))
+
+
+# **********************************************************************
+# Get cherry details from web page
+
+def getCherryDetails(cherry):
+    url = '%s/changes/?q=%s&o=CURRENT_REVISION' % (URL_GERRIT, cherry[1])
+    response = urllib.request.urlopen(url).read().decode("utf-8")
+    if response[0:4] == ")]}'":
+        response = response[4:]
+    data = json.loads(response)[0]
+    data['path'] = cherry[0]
+    return data
+
+
+# **********************************************************************
+# Validate cherry
+
+def validateCherry(data):
+    st = data['status']
+    if st == 'NEW':
+        print('Adding cherry %d %s' % (data['_number'], data['subject']))
+        return True
+    elif st == 'MERGED':
+        print('Ignoring merged cherry %d %s' % (data['_number'], data['subject']))
+        return False
+    else:
+        print('Unknown status %s for %d %s' % (data['status'], data['_number'], data['subject']))
+        return False
+
+
+# **********************************************************************
+# Main
+
+readCherries()
+
+with open(CHERRY_SCRIPT, 'w') as f:
+    f.write('#!/bin/sh\n\n')
+    f.write('set -e\n\n')
+    for cherry in gCherries:
+        data = getCherryDetails(cherry)
+        if validateCherry(data):
+            revision = data['revisions'][data['current_revision']]['fetch']['http']
+            f.write('# %s\n' % data['subject'])
+            f.write('# URL: https://gerrit.omnirom.org/%d\n' % data['_number'])
+            f.write('( cd %s; \\\n' % data['path'])
+            f.write('  git fetch %s %s && \\\n' % (revision['url'], revision['ref']))
+            f.write('  git cherry-pick FETCH_HEAD )\n\n')
+
+os.chmod(CHERRY_SCRIPT, 0o755)
+
