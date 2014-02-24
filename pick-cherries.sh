a36808d049343a1680716f7315888d21971c9e01
f69m <git+commit@f69m.de>
2014-02-24 11:23:30 +0100

    Add cherries "support old property system in recovery"

--- pick-cherries.sh
+++ pick-cherries.sh
@@ -5,12 +5,30 @@ set -e
 # [1/2] libc: support using old property system via makefile flag
 # URL: https://gerrit.omnirom.org/3389
 ( cd bionic; \
-  git fetch https://gerrit.omnirom.org/android_bionic refs/changes/89/3389/3 && \
+  git fetch https://gerrit.omnirom.org/android_bionic refs/changes/89/3389/4 && \
   git cherry-pick FETCH_HEAD )
 
 # [2/2] init: support using old property system via makefile flag
 # URL: https://gerrit.omnirom.org/3390
 ( cd system/core; \
-  git fetch https://gerrit.omnirom.org/android_system_core refs/changes/90/3390/2 && \
+  git fetch https://gerrit.omnirom.org/android_system_core refs/changes/90/3390/4 && \
+  git cherry-pick FETCH_HEAD )
+
+# [1/3] support old property system in recovery
+# URL: https://gerrit.omnirom.org/5543
+( cd bionic; \
+  git fetch https://gerrit.omnirom.org/android_bionic refs/changes/43/5543/3 && \
+  git cherry-pick FETCH_HEAD )
+
+# [3/3] support old property system in recovery
+# URL: https://gerrit.omnirom.org/5545
+( cd build; \
+  git fetch https://gerrit.omnirom.org/android_build refs/changes/45/5545/3 && \
+  git cherry-pick FETCH_HEAD )
+
+# [2/3] support old property system in recovery
+# URL: https://gerrit.omnirom.org/5544
+( cd system/core; \
+  git fetch https://gerrit.omnirom.org/android_system_core refs/changes/44/5544/3 && \
   git cherry-pick FETCH_HEAD )
 
